53224f3703b539c544cf3a36d50d9d00cfb291ed
1103-islam-md <158025236+1103-islam-md@users.noreply.github.com>
2024-05-02 11:03:33 -0700

    Create finalproject copy.c

---
+++ finalproject copy.c
@@ -0,0 +1,28 @@
+//MD / Amanda
+
+
+#include <stdio.h>
+#define size 100
+#define space ' '
+#define period '.'
+#define lower_o 'o'
+#define upper_o 'O'
+#define zero '0'
+
+void load_image()
+void display_image()
+void edit_image()
+	void crop_image()
+	void dim_brighten_image()
+void exit_image()
+void save_image()
+
+int main(){
+
+int option;
+char
+
+printf("***AMANDASGRAM***\n");
+printf("1: Load image\n2: Display image\n3: Edit image\n0: Exit\n");
+scanf("%d ", &choose);
+}
