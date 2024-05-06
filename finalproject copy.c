a3e7db4b611717b1315119b91cca08412adc6e9e
1103-islam-md <158025236+1103-islam-md@users.noreply.github.com>
2024-05-05 23:59:46 -0700

    Update finalproject copy.c

--- finalproject copy.c
+++ finalproject copy.c
@@ -9,20 +9,41 @@
 #define upper_o 'O'
 #define zero '0'
 
-void load_image()
-void display_image()
-void edit_image()
-	void crop_image()
-	void dim_brighten_image()
-void exit_image()
-void save_image()
+void load_image(int image[size][size], int *insize);
+void display_image(int image[size][size], int insize);
+void edit_image(int image[size][size], int insize);
+	void crop_image(int image[size][size], int *insize);
+	void dim_brighten_image(int image[size][size], int insize);
+void exit_image(int image[size][size], int insize);
+void save_image(int image[size][size], int insize);
 
 int main(){
 
-int option;
-char
+int options, insize = 0, image[size][size] ;
 
-printf("***AMANDASGRAM***\n");
-printf("1: Load image\n2: Display image\n3: Edit image\n0: Exit\n");
-scanf("%d ", &choose);
+do {
+	printf("***AMANDASGRAM***\n");
+	printf("1: Load image\n2: Display image\n3: Edit image\n0: Exit\n");
+	scanf("%d ", &options);
+	switch(options){
+		case 1:
+			load_image(int image[size][size], int *insize);
+			break;
+		case 2:
+			display_image(int image[size][size], int insize);
+			break;
+		case 3:
+			edit_image(int image[size][size], int insize);
+			break;
+		case 4: 
+			exit_image(int image[size][size], int insize);
+			break;
+		default: 
+			printf("Invalid input.");
+			break;
+		}
+	} while (options != 4);
+	return 0;
+
+	
 }
