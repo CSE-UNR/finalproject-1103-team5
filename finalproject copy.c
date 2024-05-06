780d528519400660c8b96952da7bffa605a8d5ba
1103-islam-md <158025236+1103-islam-md@users.noreply.github.com>
2024-05-06 10:25:23 -0700

    Update finalproject copy.c

--- finalproject copy.c
+++ finalproject copy.c
@@ -2,24 +2,24 @@
 
 
 #include <stdio.h>
-#define size 100
+#define MAX_SIZE 100
 #define space ' '
 #define period '.'
 #define lower_o 'o'
 #define upper_o 'O'
 #define zero '0'
 
-void load_image(int image[size][size], int *insize);
-void display_image(int image[size][size], int insize);
-void edit_image(int image[size][size], int insize);
-	void crop_image(int image[size][size], int *insize);
-	void dim_brighten_image(int image[size][size], int insize);
-void exit_image(int image[size][size], int insize);
-void save_image(int image[size][size], int insize);
+void load_image(char image[MAX_SIZE][MAX_SIZE], int *insize);
+void display_image(char image[MAX_SIZE][MAX_SIZE], int insize);
+void edit_image(char image[MAX_SIZE][MAX_SIZE], int insize);
+	void crop_image(char image[MAX_SIZE][MAX_SIZE], int *insize);
+	void dim_brighten_image(char image[MAX_SIZE][MAX_SIZE], int insize);
+void exit_image(char image[MAX_SIZE][MAX_SIZE], int insize);
+void save_image(char image[MAX_SIZE][MAX_SIZE], int insize);
 
 int main(){
 
-int options, options2, insize = 0, image[size][size] ;
+int options, options2, insize = 0, image[MAX_SIZE][MAX_SIZE] ;
 space = 0;
 
 do {
@@ -28,16 +28,16 @@ do {
 	scanf("%d ", &options);
 	switch(options){
 		case 1:
-			load_image(int image[size][size], int *insize);
+			//load_image(int image[size][size], int *insize);
 			break;
 		case 2:
-			display_image(int image[size][size], int insize);
+			//display_image(int image[size][size], int insize);
 			break;
 		case 3:
-			edit_image(int image[size][size], int insize);
+			edit_image(int image[MAX_SIZE][MAX_SIZE], int insize);
 			break;
 		case 4: 
-			exit_image(int image[size][size], int insize);
+			//exit_image(int image[size][size], int insize);
 			break;
 		default: 
 			printf("Invalid input.");
@@ -48,15 +48,15 @@ do {
 
 
 }
-void edit_image(int image[size][size], int insize);
+void edit_image(int image[MAX_SIZE][MAX_SIZE], int insize);
 	do {
 		printf("Choose what change you would like to make:\n1: Crop Image.\n2: Dim or Brighten Image.\n");
 		switch (options2){
 			case 1: 
-				crop_image(int image[size][size], int *insize);
+				crop_image(int image[MAX_SIZE][MAX_SIZE], int *insize);
 				break;
 			case 2:
-				dim_brighten_image(int image[size][size], int insize);
+				dim_brighten_image(int image[MAX_SIZE][MAX_SIZE], int insize);
 				break;
 			default:
 				printf("Invalid options.");
@@ -64,8 +64,12 @@ void edit_image(int image[size][size], int insize);
 		}
 		
 	}
-void crop_image(int image[size][size], int *size){
+void crop_image(int image[MAX_SIZE][MAX_SIZE], int *insize){
 	int options3;
 	printf("what size do you want the image to be?");
 	scanf("%d", &options3);
+	switch (options3){
+		case 1:
+			for(i = 0; i < size; i++)
+	}
 }
