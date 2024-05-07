5f3abfc61991f312bda812537e703eaa98efb761
1103-islam-md <158025236+1103-islam-md@users.noreply.github.com>
2024-05-06 21:09:17 -0700

    Update finalproject copy.c

--- finalproject copy.c
+++ finalproject copy.c
@@ -6,17 +6,17 @@
 #define MAX_Pixel 5
 const char pixels[] = (' ', '.', 'o', 'O', '0');
 
-void load_image(char *filename, int image[MAX_SIZE][MAX_SIZE], int *rows, int *cols);
-void display_image(int image[MAX_SIZE][MAX_SIZE], int rows, int cols);
-void edit_image(int image[MAX_SIZE][MAX_SIZE], int *rows, int *cols);
-	void crop_image(int image[MAX_SIZE][MAX_SIZE], int *rows, int *cols);
-	void dim_brighten_image(int image[MAX_SIZE][MAX_SIZE], int rows, int cols, int newpixel);
-void exit_image(int image[MAX_SIZE][MAX_SIZE], int rows, int cols);
-void save_image(char *filename, int image[MAX_SIZE][MAX_SIZE], int rows, int cols);
+void load_image(char *filename, int global_Array[MAX_SIZE][MAX_SIZE], int *rows, int *cols);
+void display_image(int global_Array[MAX_SIZE][MAX_SIZE], int rows, int cols);
+void edit_image(int global_Array[MAX_SIZE][MAX_SIZE], int *rows, int *cols);
+	void crop_image(int global_Array[MAX_SIZE][MAX_SIZE], int *rows, int *cols);
+	void dim_brighten_image(int global_Array[MAX_SIZE][MAX_SIZE], int rows, int cols, int newpixel);
+void exit_image(int global_Array[MAX_SIZE][MAX_SIZE], int rows, int cols);
+void save_image(char *filename, const int global_Array[MAX_SIZE][MAX_SIZE], int rows, int cols);
 
 int main(){
 
-int options, rows = 0, cols = 0, image[MAX_SIZE][MAX_SIZE] = {0};
+int options, rows = 0, cols = 0, global_Array[[MAX_SIZE][MAX_SIZE] = {0};
 
 do {
 	printf("***AMANDASGRAM***\n");
@@ -24,16 +24,18 @@ do {
 	scanf("%d ", &options);
 	switch(options){
 		case 1:
-			load_image(image[MAX_SIZE][MAX_SIZE], &rows, &cols);
-			break;
+			printf("Enter the file name: ");
+                scanf("%s", filename);
+                loadImage(filename, global_Array, &rows, &cols);
+                break;
 		case 2:
-			display_image(image[MAX_SIZE][MAX_SIZE], rows, cols);
+			display_image(global_Array[[MAX_SIZE][MAX_SIZE], rows, cols);
 			break;
 		case 3:
-			edit_image(int image[MAX_SIZE][MAX_SIZE], &rows, &cols);
+			edit_image(global_Array[[MAX_SIZE][MAX_SIZE], &rows, &cols);
 			break;
 		case 0: 
-			exit_image(int image[MAX_SIZE][MAX_SIZE], rows, cols);
+			exit_image(global_Array[[MAX_SIZE][MAX_SIZE], rows, cols);
 			break;
 		default: 
 			printf("Invalid input.");
@@ -44,26 +46,53 @@ do {
 
 
 }
-void edit_image(int image[MAX_SIZE][MAX_SIZE], int *rows, int *cols);{
-	int eOptions;
+void loadImage(char *filename, int global_Array[MAX_SIZE][MAX_SIZE], int *rows, int *cols) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error, unable to upload file %s\n", filename);
+        return;
+    }
+
+    fscanf(file, "%d %d", rows, cols);
+    for (int i = 0; i < *rows; i++) {
+        for (int j = 0; j < *cols; j++) {
+            fscanf(file, "%d", &image[i][j]);
+        }
+    }
+
+    fclose(file);
+    printf("Image uploaded sucessfully.\n");
+}
+void edit_image(int global_Array[MAX_SIZE][MAX_SIZE], int *rows, int *cols){
+	int eOptions2;
 	printf("***AMANDASGRAM***\nEdit:\nChoose what changes you would like to make:\n1: Crop Image.\n2: Brighten Image.\n3: Dim Image.\n");
 	scanf("%d ", eOptions2);
 	switch (eOptions2){
 		case 1: 
-			crop_image(image[MAX_SIZE][MAX_SIZE], rows, cols,);
+			crop_image(global_Array[MAX_SIZE][MAX_SIZE], rows, cols);
 			break;
 		case 2:
-			dim_brighten_image(image[MAX_SIZE][MAX_SIZE], *rows, *cols, 1);
+			dim_brighten_image(global_Array[MAX_SIZE][MAX_SIZE], *rows, *cols, 1);
 			break;
 		case 3:
-			dim_brighten_image(image[MAX_SIZE][MAX_SIZE], *rows, *cols, -1);
+			dim_brighten_image(global_Array[MAX_SIZE][MAX_SIZE], *rows, *cols, -1);
 			break;
 		default:
 			printf("Invalid options.");
 			break;
 		}
+	char save_changes;
+    printf("Would you like to save the edited image? (y/n): ");
+    scanf(" %c", &saveChoice);
+
+    if (saveChoice == 'y') {
+        char filename[100];
+        printf("Enter the filename to save the image: ");
+        scanf("%s", filename);
+        saveImage(filename, image, *rows, *cols);
+    }
 }
-void crop_image(int image[MAX_SIZE][MAX_SIZE], int *rows, int *cols){
+void crop_image(int global_Array[MAX_SIZE][MAX_SIZE], int *rows, int *cols){
 	int left, right, top, bottom;
 	printf("First left column: ");
     scanf("%d", &left);
@@ -75,11 +104,11 @@ void crop_image(int image[MAX_SIZE][MAX_SIZE], int *rows, int *cols){
     scanf("%d", &bottom);
 	int new_rows = bottom - top + 1;
     int new_cols = right - left + 1;
-    //int cropped[MAX_SIZE][MAX_SIZE] = {0};
+    int crop[MAX_SIZE][MAX_SIZE] = {0};
 
     for (int i = 0; i < new_rows; i++) {
         for (int j = 0; j < new_cols; j++) {
-            crop[i][j] = image[top + i][left + j];
+            crop[i][j] = global_Array[top + i][left + j];
         }
     }
 
@@ -88,18 +117,40 @@ void crop_image(int image[MAX_SIZE][MAX_SIZE], int *rows, int *cols){
 
     for (int i = 0; i < *rows; i++) {
         for (int j = 0; j < *cols; j++) {
-            image[i][j] = crop[i][j];
+            global_Array[i][j] = crop[i][j];
         }
     }
     printf("Image has been Cropped.\n");
 	
 }
-void dim_brighten_image(int image[MAX_SIZE][MAX_SIZE], int rows, int cols, int newpixel) {
+void dim_brighten_image(int global_Array[MAX_SIZE][MAX_SIZE], int rows, int cols, int newpixel) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            image[i][j] = (image[i][j] + newpixel + pixels) % pixels;
+            global_Array[i][j] = (global_Array[i][j] + newpixel + pixels) % pixels;
         }
     }
 
     printf("New Brightness has been set.\n");
+}
+void save_image(char *filename, const int image[MAX_SIZE][MAX_SIZE], int rows, int cols) {
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("Error uploading file: %s\n", filename);
+        return;
+    }
+
+    fprintf(file, "%d %d\n", rows, cols);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            fprintf(file, "%d ", image[i][j]);
+        }
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
+    printf("Image saved in %s.\n", filename);
+}
+void exit_image(){
+printf("Goodbye!\n");
+return 0;
 }
